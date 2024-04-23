35ccd48dca3aca21a3d8531b1ceddbb15fe66e4f
Sandeep <sandeepupadhyay1101999@gmail.com>
2024-04-23 09:50:14 +0530

    Property Contoller created

--- src/main/java/com/example/demo/controller/PropertyController.java
+++ src/main/java/com/example/demo/controller/PropertyController.java
@@ -1,5 +1,29 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Property;
+import com.example.demo.repository.PropertyRepository;
+
+@RestController
 public class PropertyController {
 
+	@Autowired
+	public PropertyRepository propertyRepository;
+	
+	@PostMapping("property")
+	public Property saveProperty(@RequestBody Property property)
+	{
+		return propertyRepository.save(property);
+	}
+	@PostMapping("/property/{id}")
+	public Property getProperty(@PathVariable("id") String property_id)
+	{
+		return propertyRepository.getPropertyById(property_id);
+	}
+	
 }
