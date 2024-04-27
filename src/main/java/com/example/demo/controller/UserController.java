f31196f63e1fb33c28dcafedf1aa47a020a299c0
Rakesh Kumar <rakesh>
2024-04-27 18:01:54 +0530

    emailId entity added to User table

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import com.example.demo.entities.User;
 import com.example.demo.repository.UserRepository;
 
 @RestController
+@CrossOrigin
 public class UserController {
 	
 	@Autowired
